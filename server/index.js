bffb40ce4a40d038b2993b85a89a670377e3c477
Vishnu PS <95961812+VishnuPScodes@users.noreply.github.com>
2023-09-18 12:53:19 +0530

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { default: axios } = require('axios');
+
+async function startServer() {
+    const app = express();
+    const server = new ApolloServer({
+        typeDefs: `
+            type Todo {
+                id:ID!,
+                title: String!
+                completed:Boolean,
+                user:User
+            }
+            type User {
+                name:String
+            }
+
+            type Query {
+                getTodos : [Todo],
+                getAllUsers :[User],
+                getUser(id:ID!):User
+            }
+        `,
+        resolvers: {
+            Todo: {
+                user: async (todo) =>
+                    (axios.get(`https://jsonplaceholder.typicode.com/users/${todo.id}`)).data
+            },
+            Query: {
+                getTodos: async () =>
+                    (await (axios.get('https://jsonplaceholder.typicode.com/todos'))).data,
+                getAllUsers: async () =>
+                    (await (axios.get('https://jsonplaceholder.typicode.com/users'))).data,
+                getUser: async (parent, { id }) =>
+                    (axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)).data
+            }
+        }
+    });
+    app.use(bodyParser.json());
+    app.use(cors());
+    await server.start();
+    app.use('/graphql', expressMiddleware(server));
+    app.listen(8000, () => console.log('server started at 8000'))
+}
+startServer()
